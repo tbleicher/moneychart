e6aecd781eab25270719675c05d4ec81a7dc3458
Thomas Bleicher <thomas@white.space>
2018-09-09 15:23:28 +0100

    spec update

--- src/testData.spec.js
+++ src/testData.spec.js
@@ -1,10 +1,8 @@
-import { generateCSVLines } from './testData';
+import { generateCSVLines } from "./testData";
 
-
-describe('generateCSVLines', () => {
-  it('generates a list of specified length', () => {
+describe("generateCSVLines", () => {
+  it("generates a list of specified length", () => {
     const lines = generateCSVLines(30);
-    expect(lines.length).toEqual(18);
+    expect(lines.length).toEqual(19);
   });
 });
-
